2c223630852dbbfc72a8c90a01f672456f469066
Brian Faggin Batista <brianfbatista@gmail.com>
2020-02-22 16:53:31 -0500

    updated with treasure hunter
    updated with treasure hunter

---
+++ Assets/Collectible.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public int treasureValue;
+
+    private void Start() 
+    {
+        treasureValue = Random.Range(1, 10);
+    }
+}
---
+++ Assets/TreasureHunter.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureHunter : MonoBehaviour
+{
+    public Collectible[] treasureList;
+    public TreasureHunterInventory inventory;
+
+    public int TreasureScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (!inventory.treasure[0])
+            {
+            inventory.treasure[0] = treasureList[0];
+            TreasureScore += inventory.treasure[0].treasureValue;
+            }
+
+            else
+            {
+                Debug.Log("Treasure 1 already collected.");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (!inventory.treasure[1])
+            {
+            inventory.treasure[1] = treasureList[1];
+            TreasureScore += inventory.treasure[1].treasureValue;
+            }
+            else
+            {
+                Debug.Log("Treasure 2 already collected.");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            if (!inventory.treasure[2])
+            {
+            inventory.treasure[2] = treasureList[2];
+            TreasureScore += inventory.treasure[2].treasureValue;
+            }
+            else
+            {
+                Debug.Log("Treasure 3 already collected.");
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Debug.Log("treasure list: " + inventory.treasure[0] + ", " + inventory.treasure[1] + ", " + inventory.treasure[2]);
+            Debug.Log("Score is: " + TreasureScore);
+
+            if (inventory.treasure[0] && inventory.treasure[1] && inventory.treasure[2])
+            {
+                Debug.Log("YOU WIN!");
+            }
+        }
+    }
+}
