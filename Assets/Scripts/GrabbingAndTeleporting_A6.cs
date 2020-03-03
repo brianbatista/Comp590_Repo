4db151d625c5004e207c33b8c5fecc4490dc5488
Brian Faggin Batista <brianfbatista@gmail.com>
2020-03-02 22:28:29 -0500

    update, assign 6
    updated

--- Assets/Scripts/GrabbingAndTeleporting_A6.cs
+++ Assets/Scripts/GrabbingAndTeleporting_A6.cs
@@ -1,9 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
-public class BFB_HandGrab_v4 : MonoBehaviour
+public class GrabbingAndTeleporting_A6 : MonoBehaviour
 {
     private float _range;
 
@@ -11,9 +12,12 @@ public class BFB_HandGrab_v4 : MonoBehaviour
 
     private GameObject[] _grabbed = new GameObject[] {null, null};
     
+    [Header("0 = Left Controller / 1 = Right Controller")]
     public GameObject[] controllers = new GameObject[2];
     private OVRInput.RawButton[] controllerTriggers = new OVRInput.RawButton[] {OVRInput.RawButton.LIndexTrigger,OVRInput.RawButton.RIndexTrigger};
     private OVRInput.RawButton[] controllerFaceButtons= new OVRInput.RawButton[] {OVRInput.RawButton.X,OVRInput.RawButton.A};
+    private OVRInput.RawButton[] controllerGrips = new OVRInput.RawButton[] {OVRInput.RawButton.LHandTrigger,OVRInput.RawButton.RHandTrigger}; // Not used :)
+    private OVRInput.RawButton teleportButton = OVRInput.RawButton.RThumbstickUp;
 
     public BeltInventory inventoryBelt;
 
@@ -26,87 +30,64 @@ public class BFB_HandGrab_v4 : MonoBehaviour
         _range = 100f; // Range for the Raycast to shoot from the hand.
     }
 
-    // Update is called once per frame
-    void Update()
-    {     
-        //for each controller
-        for (int i=0;i<2;i++){
-
-            //              Ray Debugger, if needed. Can be seen in Editor, not in game.
-            // Debug.DrawRay(controllers[i].transform.position + controllers[i].transform.forward * 1, controllers[i].transform.forward * _range, Color.green); 
-
-            //Ray from this controller
-            Ray ray = new Ray(controllers[i].transform.position + controllers[i].transform.forward * 1, controllers[i].transform.forward * _range); // Actual Ray
-            RaycastHit hit = new RaycastHit();
-
-            // Raycast Grabbing
-            if (Physics.Raycast(ray, out hit, _range,_shootableMask))
+    void Update() 
+    {    
+        for (int i=0; i<2; i++)
+        {
+            // Trigger-Ray Grab
+            if(OVRInput.Get(controllerTriggers[i]))
             {
-                Debug.Log("<color=red>Raycast has hit </color>" + hit.collider.gameObject);
-     
-                if(OVRInput.Get(controllerTriggers[i]))
+                Ray ray = new Ray(controllers[i].transform.position + controllers[i].transform.forward * 1, controllers[i].transform.forward * _range); // Actual Ray
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(ray, out hit, _range,_shootableMask))
                 {
-                    Debug.Log("<color=red>Trigger pressed on </color>" + hit.collider.gameObject);
-                    Debug.Log("Controller index i is" + i);
-                    //snap grab for this controller
+                    Debug.Log("<color=red>Trigger Grab </color> has hit " + hit.collider.gameObject);
                     grab(i,hit.collider.gameObject,true);
                 }
-
-                if(OVRInput.Get(controllerFaceButtons[i]))
-                {
-                    Debug.Log("<color=red>A/X pressed on </color>" + hit.collider.gameObject);
-                    //nosnap grab for this controller
-                    grab(i,hit.collider.gameObject,false);  
-                }
             }
-            else
+
+            // A/X Distant Grab
+            if(OVRInput.Get(controllerFaceButtons[i]))
             {
-                if(OVRInput.Get(controllerTriggers[i]))
+                Ray ray = new Ray(controllers[i].transform.position + controllers[i].transform.forward * 1, controllers[i].transform.forward * _range); // Actual Ray
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(ray, out hit, _range,_shootableMask))
                 {
-                    //I don't think spherecast necessarily returns the closest object.... acts strange for objects of different sizes. 
-                    //this is why I had a minimizer function
-                    RaycastHit magneticHit = new RaycastHit();
-                    //this should probably use controller positions.... which again can be a 2-element array
-                    if(Physics.SphereCast(controllers[i].transform.position, 100, controllers[i].transform.forward, out magneticHit, 100))
-                    {
-                        Debug.Log("<color=red>Magnetic Sphere casted.</color>" +"<color=blue> SphereCast hitted </color>" + magneticHit);
-                        //magnetic grab for this controller
-                        if(hit.collider.gameObject.layer == LayerMask.NameToLayer("Treasure"))
-                        {
-                            grab(i,magneticHit.collider.gameObject,true);
-                            Debug.Log("<color=red>Magnetic Sphere collided with </color>" + magneticHit.collider.gameObject);
-                        }
-                    }
+                    Debug.Log("<color=red>FaceButton Grab </color> has hit " + hit.collider.gameObject);
+                    grab(i,hit.collider.gameObject,false);
                 }
             }
-
-            //let go for this controller (since let go will behave the same regardless of how it was grabbed)
+            
+            // Release
             if(OVRInput.GetUp(controllerTriggers[i]) || OVRInput.GetUp(controllerFaceButtons[i]))
             {
                 Debug.Log("<color=red>Trigger or Face Button was released.</color>");
 
-                if (_grabbed[i]){
+                if (_grabbed[i])
+                {
                     // Drop into Inventory
                     var capsuleOff = new Vector3 (0.5f, 0, 0);
                     Collider[] hits = Physics.OverlapCapsule(inventoryBelt.transform.position - capsuleOff, inventoryBelt.transform.position + capsuleOff, 0.15f, _shootableMask, QueryTriggerInteraction.Collide);
+                    
+                    // If item is dropped inside Inventory
                     foreach(Collider inventoryHit in hits)
                     {
-                        if (inventoryHit.tag == "Treasure")
-                        {
+                        //if (inventoryHit.tag == "Treasure")
+                        //{
                             scoreKeeper.GetComponent<ScoreUpdater>().scoreValue += inventoryHit.GetComponent<Collectible>().treasureValue;
                             var wasHit = Resources.Load<GameObject>(inventoryHit.gameObject.GetComponent<Collectible>().ID);
                             if(inventoryBelt.inventoryDic.ContainsKey(wasHit))
-                                {
-                                    inventoryBelt.inventoryDic[wasHit]++;
-                                }
-                                else
-                                {
-                                    inventoryBelt.inventoryDic.Add(wasHit, 1);
-                                }
+                            {
+                                inventoryBelt.inventoryDic[wasHit]++;
+                            }
+                            else
+                            {
+                                inventoryBelt.inventoryDic.Add(wasHit, 1);
+                            }
 
-                                Destroy(inventoryHit.gameObject);
-                                wasHit = null;
-                        }
+                            Destroy(inventoryHit.gameObject);
+                            wasHit = null;
+                        //}
                     }
 
                     Debug.Log("<color=blue>OBJECT RELEASED.</color>");
@@ -117,10 +98,9 @@ public class BFB_HandGrab_v4 : MonoBehaviour
                     _grabbed[i] = null;
                 }
             }
-
-        
         }
     }
+
     void grab(int whichController, GameObject thingToGrab, bool shouldSnap){
         
         Debug.Log("<color=red>_grabbed</color> " + whichController + " <color=red>is</color> " + _grabbed[whichController]);
